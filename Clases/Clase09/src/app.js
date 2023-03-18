d9566b2f028afdac4c98d8bca535be434a6bc801
ElSonoSapiens <dihf.47@gmail.com>
2023-03-18 00:08:01 -0300

    clase09.02

--- Clases/Clase09/src/app.js
+++ Clases/Clase09/src/app.js
@@ -16,11 +16,59 @@ app.engine("handlebars", handlebars.engine()); // solo para HANDLEBARS o un moto
 app.set("views", __dirname + "/views"); // que setting va a configurar
 app.set("view engine", "handlebars"); // cual es el motor de plantilla
 
+// app.get("/", (req, res) => {
+// 	res.render("first")
+// });
+
+// app.get("/second", (req, res) => {
+// 	res.render("second")
+// });
+
+const users = [
+	{
+		nombre: "Diego",
+		apellido: "Hernandez",
+		edad: 33,
+		correo: "dihf.47@gmail.com",
+		telefono: 1150194222,
+	},
+	{
+		nombre: "Lisdey",
+		apellido: "Novoa",
+		edad: 31,
+		correo: "lissienovoam@gmail.com",
+		telefono: 1159059986,
+	},
+	{
+		nombre: "Barbara",
+		apellido: "Hernandez",
+		edad: 7,
+		correo: "barbarita@gmail.com",
+		telefono: 123456789,
+	},
+	{
+		nombre: "Trinidad",
+		apellido: "Hernandez",
+		edad: 6,
+		correo: "trinila@gmail.com",
+		telefono: 987654321,
+	},
+	{
+		nombre: "ElFan",
+		apellido: "Tasma",
+		edad: 99,
+		correo: "xXxXxXx@gmail.com",
+		telefono: 1122334455,
+	},
+];
+
 app.get("/", (req, res) => {
-	res.send("HANDLEBARS");
+	const indice = Math.floor(Math.random() * 4); // Código para resultados random
+	const usuario = users[indice];
+	res.render("actividad1", { ...usuario });
 });
 
 const PORT = 8080;
 app.listen(PORT, () => console.log(`escuchando puerto ${PORT}`));
 
-//me quedé en el min 48
+//me quedé en el 1:09:00
